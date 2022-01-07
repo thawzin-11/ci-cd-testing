01c1b068f6376f197f75018440787975b8536963
thawzin-11 <skydark.thawzintun@gmail.com>
2022-01-07 22:51:50 +0630

    change home page

--- zay_chin_clone_app/admin_panel/lib/main.dart
+++ zay_chin_clone_app/admin_panel/lib/main.dart
@@ -35,7 +35,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      // home: const HomePage(),
+      home: const Center(
+        child: Text('Hello Admin'),
+      ),
     );
   }
 }

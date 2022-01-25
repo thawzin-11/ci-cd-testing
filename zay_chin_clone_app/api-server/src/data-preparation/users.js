bb8ea20f579659593f3c9ad137f97ec72efefdcd
thawzin-11 <skydark.thawzintun@gmail.com>
2022-01-26 00:11:04 +0630

    add mock user in api server

--- zay_chin_clone_app/api-server/src/data-preparation/users.js
+++ zay_chin_clone_app/api-server/src/data-preparation/users.js
@@ -22,4 +22,28 @@ const user4 = {
   phNumber: "09123456789",
 };
 
-export default [user1, user2, user3, user4];
+const user5 = {
+  _id: "61824e0b3f25fcf95a38916e",
+  name: "user1",
+  phNumber: "09123456789",
+};
+
+const user6 = {
+  _id: "61824e0b3f25fcf95a38916f",
+  name: "user2",
+  phNumber: "09123456789",
+};
+
+const user7 = {
+  _id: "61824e0b3f25fcf95a389170",
+  name: "user3",
+  phNumber: "09123456789",
+};
+
+const user8 = {
+  _id: "61824e0b3f25fcf95a389171",
+  name: "user4",
+  phNumber: "09123456789",
+};
+
+export default [user1, user2, user3, user4, user5, user6, user7, user8];

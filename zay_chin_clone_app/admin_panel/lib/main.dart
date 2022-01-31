01ea5c4411d3b5b2ae8254b17f8e4b5fca569a0e
thawzin-11 <skydark.thawzintun@gmail.com>
2022-01-31 23:16:33 +0630

    some changes in admin panel

--- zay_chin_clone_app/admin_panel/lib/main.dart
+++ zay_chin_clone_app/admin_panel/lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Zay-Wal Admin Panel',
+      title: 'Zay-Wal Admin Panel App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
